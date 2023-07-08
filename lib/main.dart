5d9fc1cee1b34684b5975df307efb16130d6543f
adsoftsito <adsoft@live.com.mx>
2023-07-08 16:42:51 -0600

    add separator, url

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,47 @@ class GHFlutter extends StatefulWidget {
 }
 
 class _GHFlutterState extends State<GHFlutter> {
-  var _members = <dynamic>[];
+  //var _members = <dynamic>[];
+  final _members = <Member>[];
+
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Future<void> _loadData() async {
     const dataUrl =
         'https://api.github.com/orgs/tecnologico-de-monterrey-oficial/members';
     final response = await http.get(Uri.parse(dataUrl));
-    setState(() {
+/*    setState(() {
       _members = json.decode(response.body) as List;
       //print(_members);
     });
-  }
+*/
 
+    setState(() {
+      final dataList = json.decode(response.body) as List;
+        for (final item in dataList) {
+          final login = item['login'] as String? ?? '';
+          final url = item['avatar_url'] as String? ?? '';
+
+	  final member = Member(login, url);
+          _members.add(member);
+        } 
+    });
+
+  }
+  
   Widget _buildRow(int i) {
-    return ListTile(
-      title: Text('${_members[i]['login']}', style: _biggerFont),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListTile(
+	title: Text('${_members[i].login}', style: _biggerFont),
+	leading: CircleAvatar(
+          backgroundColor: Colors.green,
+          backgroundImage: NetworkImage(_members[i].avatarUrl),
+        ),
+      ),
     );
   }
+
 
   @override
   void initState() {
@@ -67,3 +90,10 @@ class GHFlutterApp extends StatelessWidget {
     );
   }
 }
+
+class Member {
+  Member(this.login, this.avatarUrl);
+  final String login;
+  final String avatarUrl;
+}
+
